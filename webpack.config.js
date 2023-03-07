0df320e37ab44d3ab392acb8de70807c7927659d
Ca2le <carl@jumpcode.org>
2023-03-07 22:50:26 +0100

    removed dublicates in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,6 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
-  },
-  mode: 'development',
+  }
   plugins: [new HtmlWebpackPlugin({ template: path.resolve(__dirname, './src/index.html') })],
 }
